159364d0ea31fa33d2e666155b6dc6e0a44b3c7d
Steffen Vogel <post@steffenvogel.de>
2023-05-17 16:46:29 +0200

    add first testing code
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ babel_test.go
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: 2023 Steffen Vogel <post@steffenvogel.de>
+// SPDX-License-Identifier: Apache-2.0
+
+package babel_test
+
+import (
+	"os"
+	"testing"
+
+	"golang.org/x/exp/slog"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func SetupLogging() {
+	programLevel := &slog.LevelVar{}
+	programLevel.Set(slog.LevelDebug)
+
+	handler := slog.HandlerOptions{Level: programLevel}.NewTextHandler(os.Stdout)
+	logger := slog.New(handler)
+
+	slog.SetDefault(logger)
+}
+
+func TestMain(t *testing.T) {
+	SetupLogging()
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Proto suite")
+}
