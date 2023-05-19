768c6b2018319b9b5b0a9af15bfd48f486c55251
Steffen Vogel <post@steffenvogel.de>
2023-05-19 20:37:54 +0200

    make hello history thread-safe
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- internal/history/history.go
+++ internal/history/history.go
@@ -6,6 +6,7 @@ package history
 
 import (
 	"math/bits"
+	"sync"
 
 	"github.com/stv0g/go-babel/proto"
 )
@@ -13,30 +14,43 @@ import (
 type HelloHistory struct {
 	expectedSeqno proto.SequenceNumber
 	vector        uint16
+	mu            sync.RWMutex
 }
 
 // Reset resets the history vector
 func (h *HelloHistory) Reset() {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
 	h.vector = 0
 }
 
 func (h *HelloHistory) Empty() bool {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+
 	return h.vector == 0
 }
 
 func (h *HelloHistory) Missed() (reset bool) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
 	h.vector <<= 1
 	h.expectedSeqno++
 
-	return h.Empty()
+	return h.vector == 0
 }
 
 // A.1. Maintaining Hello History
 // See: https://datatracker.ietf.org/doc/html/rfc8966#section-a.1
 func (h *HelloHistory) Update(seqno proto.SequenceNumber) (reset bool) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
 	if proto.SeqnoAbsDistance(seqno, h.expectedSeqno) > 16 {
 		reset = true
-		h.Reset()
+		h.vector = 0
 	} else if proto.SeqnoLess(seqno, h.expectedSeqno) {
 		h.vector >>= h.expectedSeqno - seqno
 	} else if proto.SeqnoLess(h.expectedSeqno, seqno) {
@@ -55,6 +69,9 @@ func (h *HelloHistory) Update(seqno proto.SequenceNumber) (reset bool) {
 // A.2.1. k-out-of-j
 // See: https://datatracker.ietf.org/doc/html/rfc8966#section-a.2.1
 func (h *HelloHistory) OutOf(k, j int) bool {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+
 	if bits.OnesCount16(h.vector<<(16-j)) >= int(k) {
 		return true
 	} else {
