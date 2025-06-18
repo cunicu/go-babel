a3daaf52954ec082f737e39699f12ac6db22036f
Steffen Vogel <post@steffenvogel.de>
2025-06-18 16:26:34 +0200

    fix(nix): Build with Go 1.24
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2023-2024 Steffen Vogel <post@steffenvogel.de>
 # SPDX-License-Identifier: Apache-2.0
-{ buildGo123Module, libpcap }:
-buildGo123Module {
+{ buildGoModule, libpcap }:
+buildGoModule {
   name = "go-babel";
   src = ./.;
   vendorHash = "sha256-IMEwQMhqMqOQ3+OM/Wp1qAIQYVoSuXLj4nqW5k6xGBY=";
